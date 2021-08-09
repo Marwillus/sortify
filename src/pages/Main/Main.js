33bcaf08c84405107beaad7b6ca2edfb1292c367
marcus <marcusxwill@gmail.com>
2021-08-09 17:17:45 +0200

    first commit

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import ListItem from "../../components/ListItem";
+
+function Main({ isValidSession, history }) {
+  const params = localStorage.getItem("params");
+  const token = JSON.parse(params).access_token;
+  const [playlists, setPlaylists] = useState([]);
+
+  console.log(playlists);
+  if (!isValidSession()) history.push("/");
+
+  useEffect(() => {
+    axios
+      .get("https://api.spotify.com/v1/me/playlists", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => setPlaylists(res.data.items))
+      .catch((err) => console.log(err));
+  }, [token]);
+
+  return (
+    <div>
+      <ul className='playlists-top'>
+        {playlists.map((item) => {
+          return <ListItem image={item.images} title={item.name} />;
+        })}
+      </ul>
+      <div className='tracklist-l'>
+        <ul>Tracklist 1</ul>
+      </div>
+      <div className='tracklist-r'>
+        <ul>Tracklist 2</ul>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
