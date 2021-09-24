5e83ca3ee4bf91a4571c9b41f5fd8d2197cda567
marcus <marcusxwill@gmail.com>
2021-09-24 16:47:37 +0200

    feat: new tracklist data structure

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { GrAddCircle } from "react-icons/gr";
 import PlaylistItem from "../../components/PlaylistItem";
 import TracklistItem from "../../components/TracklistItem";
 
@@ -8,11 +8,15 @@ function Main({ isValidSession, history }) {
   const params = localStorage.getItem("params");
   const token = JSON.parse(params).access_token;
   const [playlists, setPlaylists] = useState([]);
-  const [tracklistLeft, setTracklistLeft] = useState([]);
-  const [tracklistRight, setTracklistRight] = useState([]);
+  const [tracklists, setTracklists] = useState([
+    { list: [], loading: false },
+    { list: [], loading: false },
+  ]);
+  // const [tracklistLeft, setTracklistLeft] = useState([]);
+  // const [tracklistRight, setTracklistRight] = useState([]);
 
   if (!isValidSession()) history.push("/");
-// console.log(playlists);
+  // console.log(playlists);
   useEffect(() => {
     axios
       .get("https://api.spotify.com/v1/me/playlists", {
@@ -24,40 +28,101 @@ function Main({ isValidSession, history }) {
       .catch((err) => console.log(err));
   }, [token]);
 
-  const getPlaylist = (url, left) => {
+  const getPlaylist = (url, column) => {
     axios
-    .get(url, {
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    })
-    .then((res) => left?setTracklistLeft(res.data.items):setTracklistRight(res.data.items))
-    .catch((err) => console.log(err));
-  }
+      .get(url, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        setTracklists((prevLists) => {
+          return prevLists.map((tracklist, i) => {
+            if (i === column)  return { ...tracklist, list: res.data.items };
+             return tracklist;
+          });
+        });
+        console.log(tracklists);
+      })
+      .catch((err) => console.log(err));
+  };
   // console.log(tracklistLeft);
   return (
     <>
       <div className="playlists-top">
         {playlists.map((item) => {
-          return (<div onClick={()=>getPlaylist(item.tracks.href, true)} onDoubleClick={()=>getPlaylist(item.tracks.href, false)}>
-          <PlaylistItem image={item.images} title={item.name} key={item.id} />;
-          </div>)
+          return (
+            <div
+              onClick={() => getPlaylist(item.tracks.href, 0)}
+              onDoubleClick={() => getPlaylist(item.tracks.href, 1)}
+              key={item.id}
+            >
+              <PlaylistItem image={item.images} title={item.name} />;
+            </div>
+          );
         })}
       </div>
       <div className="tracklist-container">
-        <div className="tracklist">
-          {tracklistLeft.map((item)=>{
-            console.log(item);
-            return   <TracklistItem key={item.track.id} image={item.track.album.images} title={item.track.name} artists={item.track.artists}/>
-          })}
-        </div>
-        <div className="tracklist">
-        {tracklistRight.map((item)=>{
-            console.log(item);
-            return   <TracklistItem key={item.track.id} image={item.track.album.images} title={item.track.name} artists={item.track.artists}/>
-          })}
-        </div>
+        {tracklists.map((trackobj, i) => {
+          return (
+            <div key={"tracklist-" + i} className="tracklist">
+              {trackobj.list.length > 0 ? (
+                trackobj.list.map((item) => {
+                  console.log(item);
+                  return (
+                    <TracklistItem
+                      key={item.track.id}
+                      image={item.track.album.images}
+                      title={item.track.name}
+                      artists={item.track.artists}
+                    />
+                  );
+                })
+              ) : (
+                <div className="tracklist-placeholder">
+                  {" "}
+                  Drop a Playlist <GrAddCircle />
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
+
+      {/* <div className="tracklist">
+          {tracklistLeft.length > 0 ? (
+            tracklistLeft.map((item) => {
+              console.log(item);
+              return (
+                <TracklistItem
+                  key={item.track.id}
+                  image={item.track.album.images}
+                  title={item.track.name}
+                  artists={item.track.artists}
+                />
+              );
+            })
+          ) : (
+            <div className="tracklist-placeholder">
+              {" "}
+              Drop a Playlist <GrAddCircle />
+            </div>
+          )}
+        </div>
+        <div className="tracklist">
+          {tracklistRight.map((item) => {
+            console.log(item);
+            return (
+              <TracklistItem
+                key={item.track.id}
+                image={item.track.album.images}
+                title={item.track.name}
+                artists={item.track.artists}
+              />
+            );
+          })}
+        </div>
+      </div> */}
     </>
   );
 }
